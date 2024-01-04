af6beff970fb5497e8bce5702807edb873e89b82
AdriAlfonso <adriaalfonsomedero@gmail.com>
2024-01-04 16:09:30 +0100

    feature(): handle some argument errors (CustomArgumentParser)

--- fsl-pydocker.py
+++ fsl-pydocker.py
@@ -1,7 +1,13 @@
 import argparse
 import docker
 from colorama import Fore, Style, init
+import sys
+
 init(autoreset=True)
+
+class CustomArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        raise argparse.ArgumentError(None, message)
 
 def run_container(local_fsl_data_path):
     try:
@@ -19,7 +25,7 @@ def run_container(local_fsl_data_path):
             'tty': True,
             'stdin_open': True,
             'detach': True,
-            'working_dir': '/fsl-pydocker', 
+            'working_dir': '/fsl-pydocker',
             'command': '/bin/bash',
             'volumes': {local_fsl_data_path: {'bind': container_fsl_data_path, 'mode': 'rw'}}
             # Mount the local directory into the container with read and write permissions
@@ -32,7 +38,7 @@ def run_container(local_fsl_data_path):
         ls_result = container.exec_run(['ls', container_fsl_data_path])
         print(f"Content of directory {container_fsl_data_path} in the container:")
         print(ls_result.output.decode('utf-8'))
-        
+
         # Display the FSL version in the container
         fsl_version = container.exec_run(['cat', '/usr/share/fsl/5.0/etc/fslversion'])
         print('Welcome to FSL!' + ' -> ' + fsl_version.output.decode('utf-8'))
@@ -41,20 +47,30 @@ def run_container(local_fsl_data_path):
         print(f"{Fore.RED}An error occurred: {str(e)}{Style.RESET_ALL}")
 
     finally:
-        # Stop and remove the container once the operations are done
         container.stop()
         container.remove()
 
-if __name__ == "__main__":
-     # Parse the command-line arguments
-    parser = argparse.ArgumentParser(description=f'{Fore.CYAN}(HELP PANEL) -> Run FSL Docker container:{Style.RESET_ALL}')
+def parse_args(args):
+    # Use the custom parser to suppress the default error message
+    parser = CustomArgumentParser(
+        description=f'{Fore.CYAN}(HELP PANEL) -> Run FSL Docker container: python -m fsl-pydocker -v VOLUME_PATH{Style.RESET_ALL}'
+    )
 
-    # Add argument for local path
+    # Add argument for local volume path
     parser.add_argument('-v', '--volume-path', type=str, required=True,
                         help=f'{Fore.YELLOW}Local path to FSL data volume{Style.RESET_ALL}')
 
-    # Parse the arguments or show help if not provided
-    args = parser.parse_args()
+    try:
+        return parser.parse_args(args)
+
+    except argparse.ArgumentError as e:
+        print(f"{Fore.RED}Error: {e}{Style.RESET_ALL}")
+        parser.print_help()
+        sys.exit(1)
+
+if __name__ == "__main__":
+    # Parse the command-line arguments
+    args = parse_args(sys.argv[1:])
 
     # Run the container with the specified local path
     run_container(args.volume_path)
