265afbf31518e65307d19cc6c198c015e7a3af23
AdriAlfonso <adriaalfonsomedero@gmail.com>
2024-01-05 17:39:28 +0100

    feature(): fsl container now running using subprocess + .nii for testing

--- fsl-pydocker.py
+++ fsl-pydocker.py
@@ -1,14 +1,15 @@
 import argparse
-import docker
+import subprocess
 from colorama import Fore, Style, init
 import sys
 
+# Initialize colorama for colored terminal output
 init(autoreset=True)
 
 class CustomArgumentParser(argparse.ArgumentParser):
     def error(self, message):
         raise argparse.ArgumentError(None, message)
-    
+
 def print_fsl_banner():
     text = [
         f"{Fore.BLUE}  _____ ____  _               ______   ______   ___   ____ _  _______ ____  {Style.RESET_ALL}",
@@ -20,87 +21,49 @@ def print_fsl_banner():
 
     for line in text:
         print(line)
-    
+
     print("\n")
+
+    print(Fore.GREEN + 'Welcome to FSL! (5.0.9)')
+    print("fsl-pydocker is a Python script for running FSL (FMRIB Software Library) in a Docker container.")
+    print("It allows you to perform neuroimaging analysis using FSL without the need to install FSL locally.")
+    print("Type 'exit' if you want to stop the fsl-pydocker container:\n")
 
 def print_version_info():
     print(f"fsl-pydocker 1.0")
     print(f"Python Version: {sys.version}")
-    print(f"Docker Version: {docker.__version__}")
+    docker_version = subprocess.run(['docker', '--version'], capture_output=True, text=True)
+    print(f"Docker Version: {docker_version.stdout.strip()}")
 
 def run_container(local_fsl_data_path):
+    print_fsl_banner()
     try:
-        # Create a Docker client
-        client = docker.from_env()
+        # Run 'docker run' command using subprocess to start the FSL container
+        subprocess.run(['docker', 'run', '-it', '--rm','-w', '/volume/', '-v', f'{local_fsl_data_path}:/volume/', 'brainlife/fsl', '/bin/bash'], check=True)
 
-        # Specify the Docker image to use
-        image = "brainlife/fsl"
-
-        # Directory inside the container where the volume will be mounted
-        container_fsl_data_path = "/volume/"
-
-        # Container configuration
-        container_config = {
-            'tty': True,
-            'stdin_open': True,
-            'detach': True,
-            'working_dir': '/fsl-pydocker',
-            'command': '/bin/bash',
-            'volumes': {local_fsl_data_path: {'bind': container_fsl_data_path, 'mode': 'rw'}}
-            # Mount the local directory into the container with read and write permissions
-        }
-
-        # Run the container with the specified volume
-        container = client.containers.run(image, **container_config)
-
-        # Execute 'ls' command inside the container to list the content of the specified directory
-        ls_result = container.exec_run(['ls', container_fsl_data_path])
-        print("\n" + f"Content of directory {container_fsl_data_path} inside the container:")
-        print(ls_result.output.decode('utf-8'))
-
-        # Tool description
-        print_fsl_banner()
-        print(Fore.GREEN + 'Welcome to FSL! (5.0.9)')
-        print("fsl-pydocker is a Python script for running FSL (FMRIB Software Library) in a Docker container.")
-        print("It allows you to perform neuroimaging analysis using FSL without the need to install FSL locally.")
-
-        # Main loop
-        print("\nEnter a command to run inside the container (type 'exit' to exit) ")
-        while True:
-            user_command = input("fsl-pydocker@root $ ")
-            if user_command.lower() == 'exit':
-                break
-            else:
-                command_result = container.exec_run(user_command)
-                print(command_result.output.decode('utf-8'))
-
-    except Exception as e:
-        print(f"{Fore.RED}An error occurred: {str(e)}{Style.RESET_ALL}")
-
-    finally:
-        container.stop()
-        container.remove()
+    except subprocess.CalledProcessError as e:
+        print(f"{Fore.RED}An error occurred: {e}{Style.RESET_ALL}")
 
 def parse_args(args):
-    # Use the custom parser to suppress the default error message
+    # Create a custom ArgumentParser with specific formatting
     parser = CustomArgumentParser(
         description=f'{Fore.CYAN}(HELP PANEL) -> Run FSL Docker container: python -m fsl-pydocker -v VOLUME_PATH{Style.RESET_ALL}'
     )
 
-    # Add argument for local volume path
+    # Define command-line arguments
     parser.add_argument('-v', '--volume-path', type=str, nargs='?',
                         help=f'{Fore.YELLOW}Local path to FSL data volume{Style.RESET_ALL}')
-    # New options for version information
     parser.add_argument('--version', action='store_true', help='Show version information')
 
     try:
         args = parser.parse_args(args)
 
+        # Check for missing required arguments
         if not args.volume_path and not args.version:
-            # If no arguments are provided, show help
             parser.print_help()
             sys.exit(1)
 
+        # Display version information and exit if requested
         if args.version:
             print_version_info()
             sys.exit(0)
@@ -113,8 +76,6 @@ def parse_args(args):
     return args
 
 if __name__ == "__main__":
-    # Parse the command-line arguments
+    # Parse command-line arguments and run the FSL Docker container
     args = parse_args(sys.argv[1:])
-
-    # Run the container with the specified local path
     run_container(args.volume_path)
