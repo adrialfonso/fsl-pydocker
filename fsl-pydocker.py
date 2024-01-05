012f77712d2fca49cb91ca6cea9a9eeef21de59a
AdriAlfonso <adriaalfonsomedero@gmail.com>
2024-01-05 18:17:45 +0100

    feature(): utils.py creation

--- fsl-pydocker.py
+++ fsl-pydocker.py
@@ -1,7 +1,7 @@
 import argparse
-import subprocess
-from colorama import Fore, Style, init
 import sys
+from colorama import Fore, Style, init
+from utils import print_fsl_banner, print_version_info, run_command
 
 # Initialize colorama for colored terminal output
 init(autoreset=True)
@@ -10,39 +10,10 @@ class CustomArgumentParser(argparse.ArgumentParser):
     def error(self, message):
         raise argparse.ArgumentError(None, message)
 
-def print_fsl_banner():
-    text = [
-        f"{Fore.BLUE}  _____ ____  _               ______   ______   ___   ____ _  _______ ____  {Style.RESET_ALL}",
-        f"{Fore.BLUE} |  ___/ ___|| |             |  _ \\ \\ / /  _ \\ / _ \\ / ___| |/ /| ____|  _ \\ {Style.RESET_ALL}",
-        f"{Fore.BLUE} | |_  \\___ \\| |      _____  | |_) \\ V /| | | | | | | |   | ' / |  _| | |_) |{Style.RESET_ALL}",
-        f"{Fore.BLUE} |  _|  ___) | |___  |_____| |  __/ | | | |_| | |_| | |___| . \\ | |___|  _ < {Style.RESET_ALL}",
-        f"{Fore.BLUE} |_|   |____/|_____|         |_|    |_| |____/ \\___/ \\____|_|\\_\\|_____|_| \\_\\{Style.RESET_ALL}",
-    ]
-
-    for line in text:
-        print(line)
-
-    print("\n")
-
-    print(Fore.GREEN + 'Welcome to FSL! (5.0.9)')
-    print("fsl-pydocker is a Python script for running FSL (FMRIB Software Library) in a Docker container.")
-    print("It allows you to perform neuroimaging analysis using FSL without the need to install FSL locally.")
-    print("Type 'exit' if you want to stop the fsl-pydocker container:\n")
-
-def print_version_info():
-    print(f"fsl-pydocker 1.0")
-    print(f"Python Version: {sys.version}")
-    docker_version = subprocess.run(['docker', '--version'], capture_output=True, text=True)
-    print(f"Docker Version: {docker_version.stdout.strip()}")
-
 def run_container(local_fsl_data_path):
     print_fsl_banner()
-    try:
-        # Run 'docker run' command using subprocess to start the FSL container
-        subprocess.run(['docker', 'run', '-it', '--rm','-w', '/volume/', '-v', f'{local_fsl_data_path}:/volume/', 'brainlife/fsl', '/bin/bash'], check=True)
-
-    except subprocess.CalledProcessError as e:
-        print(f"{Fore.RED}An error occurred: {e}{Style.RESET_ALL}")
+    # Run 'docker run' command using run_command to start the FSL container
+    run_command(['docker', 'run', '-it', '--rm','-w', '/volume/', '-v', f'{local_fsl_data_path}:/volume/', 'brainlife/fsl', '/bin/bash'])
 
 def parse_args(args):
     # Create a custom ArgumentParser with specific formatting
---
+++ utils.py
@@ -0,0 +1,34 @@
+from colorama import Fore, Style
+import subprocess
+import sys
+
+def run_command(command, check=True):
+    try:
+        subprocess.run(command, check=check)
+    except subprocess.CalledProcessError as e:
+        print(f"{Fore.RED}An error occurred: {e}{Style.RESET_ALL}")
+
+def print_fsl_banner():
+    text = [
+        f"{Fore.BLUE}  _____ ____  _               ______   ______   ___   ____ _  _______ ____  {Style.RESET_ALL}",
+        f"{Fore.BLUE} |  ___/ ___|| |             |  _ \\ \\ / /  _ \\ / _ \\ / ___| |/ /| ____|  _ \\ {Style.RESET_ALL}",
+        f"{Fore.BLUE} | |_  \\___ \\| |      _____  | |_) \\ V /| | | | | | | |   | ' / |  _| | |_) |{Style.RESET_ALL}",
+        f"{Fore.BLUE} |  _|  ___) | |___  |_____| |  __/ | | | |_| | |_| | |___| . \\ | |___|  _ < {Style.RESET_ALL}",
+        f"{Fore.BLUE} |_|   |____/|_____|         |_|    |_| |____/ \\___/ \\____|_|\\_\\|_____|_| \\_\\{Style.RESET_ALL}",
+    ]
+
+    for line in text:
+        print(line)
+
+    print("\n")
+
+    print(Fore.GREEN + 'Welcome to FSL! (5.0.9)')
+    print("fsl-pydocker is a Python script for running FSL (FMRIB Software Library) in a Docker container.")
+    print("It allows you to perform neuroimaging analysis using FSL without the need to install FSL locally.")
+    print("From now on, you're on a Ubuntu 16.04.3 LTS container. Type 'exit' if you want to stop fsl-pydocker:\n")
+
+def print_version_info():
+    print(f"fsl-pydocker 1.0")
+    print(f"Python Version: {sys.version}")
+    docker_version = subprocess.run(['docker', '--version'], capture_output=True, text=True)
+    print(f"Docker Version: {docker_version.stdout.strip()}")
